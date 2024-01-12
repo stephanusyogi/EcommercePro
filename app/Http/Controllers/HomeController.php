fe38aada19aa653a4f74330a4499b5ed755ce5cb
stephanusyogi <stephanusyogi12@gmail.com>
2024-01-12 18:49:46 +0700

    First Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('home.userpage');
+    }
+
+    public function redirect()
+    {
+        $usertype = Auth::User()->usertype;
+        if ($usertype == '1') {
+            return view('admin.home');
+        } else {
+            return view('home.userpage');
+        }
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      @include('admin.header')
+        <!-- partial -->
+      @include('admin.body')
+    <!-- container-scroller -->
+    @include('admin.script')
+  </body>
+</html>
